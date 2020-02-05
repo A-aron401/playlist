ba1dc45136017fc1953d86ced192571943b5ce2c
aaliyah joseph <s_aaliyah.joseph@ousd.org>
2020-02-05 02:23:55 +0000

    added some css and created divs for the categories.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var songNames=['Guilty Conscience','Potential','All for us','SUGAR','everything i wanted','Say So','Garden','Glitter','Paradise','Doin Time','Recess','Come Thru'];
+ 
+songNames.forEach(function(songs) {
+    $(".songName").display();
+}
